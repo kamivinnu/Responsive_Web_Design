0d4a776dd86a6084c779a09616c1440868ba5808
kamivinnu <you@example.com>
2025-01-22 18:36:02 +0530

    ten

---
+++ 16_css_magazine/dom.php
@@ -0,0 +1,42 @@
+Document
+│
+├── <html> lang="en"
+│   ├── <head>
+│   │   ├── <meta charset="UTF-8">
+│   │   ├── <meta name="viewport" content="width=device-width, initial-scale=1.0">
+│   │   ├── <title>Magazine</title>
+│   │   ├── <link> (Google Fonts)
+│   │   ├── <link> (FontAwesome)
+│   │   ├── <link> (styles.css)
+│   ├── <body>
+│       ├── <main>
+│       │   ├── <section class="heading">
+│       │   │   ├── <header class="hero">
+│       │   │   |   ├── <img> (Hero Image)
+│       │   │   │   ├── <h1>OUR NEW CURRICULUM</h1>
+│       │   │   │   ├── <p>Subtitle</p>
+│       │   │   ├── <div class="author">
+│       │   │   │   ├── <p>Author Name</p>
+│       │   │   │   ├── <p>Publish Date</p>
+│       │   │   ├── <div class="social-icons">
+│       │   │   │   ├── <a> (Facebook)
+│       │   │   │   ├── <a> (Twitter)
+│       │   │   │   ├── <a> (Instagram)
+│       │   │   │   ├── <a> (LinkedIn)
+│       │   │   │   ├── <a> (YouTube)
+│       │   ├── <section class="text">
+│       │   │   ├── <p>First paragraph</p>
+│       │   │   ├── <p>Additional content...</p>
+│       │   │   ├── <blockquote>Quote</blockquote>
+│       │   ├── <section class="text text-with-images">
+│       │   │   ├── <article class="brief-history">
+│       │   │   │   ├── <h3>A Brief History</h3>
+│       │   │   │   ├── <ul class="lists">
+│       │   │   │   │   ├── <li>V1 - 2014</li>
+│       │   │   │   │   ├── <li>V2 - 2015</li>
+│       │   │   │   │   ├── ... (more versions)
+│       │   │   ├── <aside class="image-wrapper">
+│       │   │   │   ├── <img> (Quote Machine)
+│       │   │   │   ├── <img> (Calculator)
+│       │   │   │   ├── <blockquote>Image Quote</blockquote>
+│       │   │   │   ├── <img> (Survey Form)
